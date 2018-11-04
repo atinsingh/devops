938e0513c5f7e444dd4f399809b903f055c5f8d0
Atin Singh <P@ssw0rd>
2018-11-04 10:54:29 -0500

    Added new files

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
+
+var cc = require("./creditcard.js");
 var name =  "DevOps Master class";
 console.log(name);
 greetMe("Hello2");
 
-<<<<<<< HEAD
 //comment
 function greetMe(greeting){
 	console.log(greeting);
 }
-=======
->>>>>>> 9bf98af9af78aa0722e3cfec4c6b6b40e081c6b3
 //added comment
 
 function test() {
